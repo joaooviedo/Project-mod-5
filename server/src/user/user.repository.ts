c5b98f2303cc30d973dd88eaffb28fc0fd0f5421
joaooviedo <joaovitorgiroldi@gmail.com>
2022-12-14 21:28:35 -0300

    Feat: refactor errors

--- server/src/user/user.repository.ts
+++ server/src/user/user.repository.ts
@@ -3,21 +3,30 @@ import { IUserEntity } from './entities/user.entity';
 import { PartialUserDto } from './services/dto/partialUserImport.dto';
 import { Injectable } from '@nestjs/common';
 import { Exceptions } from 'src/utils/exceptions/exceptionsHelper';
+import { Exception } from 'src/utils/exceptions/exception';
 
 @Injectable()
 export class UserRepository {
   constructor(private readonly prisma: PrismaService) {}
 
   async findAllUsers(): Promise<IUserEntity[]> {
-    const AllUsers = await this.prisma.user.findMany();
-    return AllUsers;
+    try {
+      const allUsers = await this.prisma.user.findMany();
+      return allUsers;
+    } catch (err) {
+      throw new Exception(Exceptions.DatabaseException);
+    }
   }
 
   async findUserById(id: string): Promise<IUserEntity> {
-    const FoundUser = await this.prisma.user.findUniqueOrThrow({
-      where: { id: id },
-    });
-    return FoundUser;
+    try {
+      const foundUser = await this.prisma.user.findUniqueOrThrow({
+        where: { id: id },
+      });
+      return foundUser;
+    } catch (err) {
+      throw new Exception(Exceptions.DatabaseException);
+    }
   }
 
   async createUser(user: IUserEntity): Promise<IUserEntity> {
@@ -25,25 +34,36 @@ export class UserRepository {
       const CreatedUser = await this.prisma.user.create({ data: user });
       return CreatedUser;
     } catch (err) {
-      throw {
-        message: 'User, CPF or email already registered',
-        exception: Exceptions.DatabaseException,
-      };
+      throw new Exception(
+        Exceptions.DatabaseException,
+        'User, CPF or email already registered.',
+      );
     }
   }
 
   async updateUser(user: PartialUserDto): Promise<IUserEntity> {
-    const UpdatedUser = await this.prisma.user.update({
-      where: { id: user.id },
-      data: user,
-    });
-    return UpdatedUser;
+    try {
+      const UpdatedUser = await this.prisma.user.update({
+        where: { id: user.id },
+        data: user,
+      });
+      return UpdatedUser;
+    } catch (err) {
+      throw new Exception(Exceptions.DatabaseException);
+    }
   }
 
   async deleteUser(id: string): Promise<IUserEntity> {
-    const DeletedUser = await this.prisma.user.delete({
-      where: { id: id },
-    });
-    return DeletedUser;
+    try {
+      const deletedUser = await this.prisma.user.delete({
+        where: { id: id },
+      });
+      return deletedUser;
+    } catch (err) {
+      throw new Exception(
+        Exceptions.DatabaseException,
+        'User not found in database',
+      );
+    }
   }
 }
